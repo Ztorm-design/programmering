4393a9fb126549230fb4849484e019f3f622562d
unknown <ht24cast@axevalla.fhsk.se>
2024-09-12 12:04:03 +0200

    sub repsotority1

---
+++ C# Uppgift 2 CS/C# Uppgift 2 CS/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string namn;
+}
+    Console.writeline ("Välkommen till mitt andra program")
+     
+    Console.readline()
